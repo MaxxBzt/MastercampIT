522e8c44dca2c947a2b3dbab05678a4b85750800
Maxime Bézot <60543092+MaxxBzt@users.noreply.github.com>
2024-06-03 16:12:27 +0200

    Vertices done from data 1

---
+++ ExtractData.py
@@ -0,0 +1,40 @@
+
+# # format pour les sommets :
+# V num_sommet nom_sommet numéro_ligne si_terminus branchement (0 stations en commun, 1 pour la direction 1,  2 pour la direction 2, ainsi
+#   de suite ...)
+#
+# # format pour les arrêtes :
+# E num_sommet1 num_sommet2 temps_en_secondes
+
+import networkx as nx
+def dataversion1():
+    print("Data version 1")
+    file = open("Version1/metro.txt", "r")
+    # start at line 15
+    for i in range(15):
+        file.readline()
+    # Create all the nodes
+    G = nx.Graph()
+    for line in file:
+        if line[0] == 'V':
+            # remove \n
+            line = line[:-1]
+            # remove V
+            line = line[2:]
+            line = line.split(";")
+            name_stop = line[0].strip().split()[1:]
+            name_stop = ' '.join(name_stop)
+            G.add_node(int(line[0].split(" ")[0]),name=name_stop,ligne=str(line[1]).strip(),if_terminus=bool(line[2].split(" ")[0].strip()),branchement=int(line[2].split(" ")[1]))
+
+    # Create all the edges
+    for line in file:
+        if line[0] == 'E':
+            line = line.split()
+            G.add_edge(int(line[1]), int(line[2]), duration=int(line[3]))
+
+    return G
+
+G = dataversion1()
+for i in G.nodes:
+    print(G.nodes[i])
+
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import matplotlib.pyplot as plt
+
+
 print("Hello world")
+import networkx as nx
+G = nx.Graph()
+
+G.add_node(1,name="Abesses",ligne=12,if_terminus=False,branchement=0)
+G.add_node(3,name="Alésia",ligne=4,if_terminus=False,branchement=0)
+
+print(G.nodes[1])
+
+
+
+
+G.add_edge(1, 3, duration=2)
+
+G.number_of_nodes()
+G.number_of_edges()
+print(list(G.nodes))
+print(list(G.edges))
+
+
