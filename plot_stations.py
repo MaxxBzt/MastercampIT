ae2b4d5a79c8bf6cbe58357a6bdb5d10a0118084
Rolwen <n.rnolwen@gmail.com>
2024-06-10 01:45:19 +0200

    Revert "End of projects (with the builds"
    This reverts commit 583f7ba98dc9081ed0a6399b556e29b35d656f35.

--- plot_stations.py
+++ plot_stations.py
@@ -1,39 +1,35 @@
 import pandas as pd
-from PIL import Image, ImageDraw, ImageTk
+from PIL import Image, ImageDraw
 import os
 
-def plot_stations_on_canvas(canvas, file_path):
-    version1_path = os.path.join(os.getcwd(), 'Version1')
-    if os.path.isdir(version1_path):
-        print("Contents of Version1 directory:", os.listdir(version1_path))
+
+file_path = 'Version1\pospoints.txt'
+
+version1_path = os.path.join(os.getcwd(), 'Version1')
+if os.path.isdir(version1_path):
+    print("Contents of Version1 directory:", os.listdir(version1_path))
+else:
+    print("The Version1 directory does not exist.")
+
+if not os.path.isfile(file_path):
+    print(f"Le fichier {file_path} n'a pas été trouvé. Vérifiez le chemin du fichier.")
+else:
+    data = pd.read_csv(file_path, delimiter=';', header=None, names=['x', 'y', 'station'])
+
+    data['station'] = data['station'].str.replace('@', ' ')
+
+    print("First 5 rows of the DataFrame:")
+    print(data.head())
+
+    map_image_path = 'paris_map.png.jpg'  
+    if not os.path.isfile(map_image_path):
+        print(f"L'image de la carte {map_image_path} n'a pas été trouvée. Vérifiez le chemin de l'image.")
     else:
-        print("The Version1 directory does not exist.")
-    
-    if not os.path.isfile(file_path):
-        print(f"Le fichier {file_path} n'a pas été trouvé. Vérifiez le chemin du fichier.")
-    else:
-        data = pd.read_csv(file_path, delimiter=';', header=None, names=['x', 'y', 'station'])
+        map_image = Image.open(map_image_path)
+        draw = ImageDraw.Draw(map_image)
 
-        data['station'] = data['station'].str.replace('@', ' ')
+        for index, row in data.iterrows():
+            x, y = row['x'], row['y']
+            draw.ellipse((x-5, y-5, x+5, y+5), fill='blue', outline='blue')
 
-        print("First 5 rows of the DataFrame:")
-        print(data.head())
-
-        map_image_path = 'paris_map.png.jpg'  
-        if not os.path.isfile(map_image_path):
-            print(f"L'image de la carte {map_image_path} n'a pas été trouvée. Vérifiez le chemin de l'image.")
-        else:
-            map_image = Image.open(map_image_path)
-            draw = ImageDraw.Draw(map_image)
-
-            for index, row in data.iterrows():
-                x, y = row['x'], row['y']
-                draw.ellipse((x-5, y-5, x+5, y+5), fill='blue', outline='blue')
-
-            # Convert PIL image to Tkinter PhotoImage
-            map_photo = ImageTk.PhotoImage(map_image)
-            canvas.image = map_photo  # Keep a reference to prevent garbage collection
-            canvas.create_image(0, 0, anchor='nw', image=map_photo)
-
-            # Notify Tkinter to redraw the canvas
-            canvas.update()
+        map_image.show()  
