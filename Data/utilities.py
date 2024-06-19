8affb0bd395774bf19493fc6fe5a97afb3c8780b
Rolwen <n.rnolwen@gmail.com>
2024-06-19 14:33:06 +0200

    Created a dictionary with the path to the metro lines images and their color

---
+++ Data/utilities.py
@@ -0,0 +1,19 @@
+metro_lines_info = {
+    "1": ("Metro_lines_images/paris_metro_line_1.png", "#FFCD00"),   # Line 1 (Yellow)
+    "2": ("Metro_lines_images/paris_metro_line_2.png", "#003CA6"),   # Line 2 (Blue)
+    "3": ("Metro_lines_images/paris_metro_line_3.png", "#837902"),   # Line 3 (Olive Green)
+    "3bis" : ("Metro_lines_images/paris_metro_line_3bis.png", "#7BAC40"),  # Line 3bis (Light Green)
+    "4": ("Metro_lines_images/paris_metro_line_4.png", "#CF009E"),   # Line 4 (Fuchsia)
+    "5": ("Metro_lines_images/paris_metro_line_5.png", "#FF7E2E"),   # Line 5 (Orange)
+    "6": ("Metro_lines_images/paris_metro_line_6.png", "#6EC4E8"),   # Line 6 (Light Blue)
+    "7": ("Metro_lines_images/paris_metro_line_7.png", "#FA9ABA"),   # Line 7 (Pink)
+    "7bis": ("Metro_lines_images/paris_metro_line_7bis.png", "#6ECA97"),  # Line 7bis (Light Green)
+    "8": ("Metro_lines_images/paris_metro_line_8.png", "#E19BDF"),   # Line 8 (Lilac)
+    "9": ("Metro_lines_images/paris_metro_line_9.png", "#B7A8D3"),   # Line 9 (Purple)
+    "10": ("Metro_lines_images/paris_metro_line_10.png", "#C9910D"),  # Line 10 (Mustard Yellow)
+    "11": ("Metro_lines_images/paris_metro_line_11.png", "#704B1C"),  # Line 11 (Brown)
+    "12": ("Metro_lines_images/paris_metro_line_12.png", "#007852"),  # Line 12 (Emerald Green)
+    "13": ("Metro_lines_images/paris_metro_line_13.png", "#6EC4E8"),  # Line 13 (Cobalt Blue)
+    "14": ("Metro_lines_images/paris_metro_line_14.png", "#62259D"),  # Line 14 (Magenta)
+}
+
